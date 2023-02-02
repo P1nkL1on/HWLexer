42666af3c8b12a9126292b744435d45c6c0b1a16
P1nkL1on <lion6149@mail.ru>
2023-02-03 00:20:58 +0400

    Added token stack and err handling for lexer

--- main.cpp
+++ main.cpp
@@ -10,6 +10,22 @@ enum {
   ERR_NO,
   ERR_CANT_READ_FILE,
   ERR_FILE_IS_EMPTY,
+  ERR_TOKEN_UNDEFINED,
+  ERR_TOKEN_AMBIGIOUS,
+};
+
+constexpr int n_keywords = 24;
+const std::array<std::string, n_keywords> token_strs{
+    "+", "+=", "!",    "!=",     "=",  "==",    "(",     ")",
+    "{", "}",  "func", "return", "if", "while", "print", "else",
+    "?", ":",  ";",    "&&",     "-",  "*",     "/",     "||",
+};
+constexpr int n_tokens = n_keywords + 2;
+constexpr int ind_token_num = n_keywords + 0;
+constexpr int ind_token_id =  n_keywords + 1;
+const std::string token_id_stops = "+=!(){}?:;&|-*/";
+inline bool is_id(const char c) { 
+  return token_id_stops.find(c) == std::string::npos; 
 };
 
 struct Token
@@ -18,7 +34,6 @@ struct Token
   int col = 0; // aka line pos index + 1
   int row = 0; // aka line index + 1
   enum {
-    UNDEFINED = -1,
     PLUS,
     ADDITION_ASSIGNMENT,
     NOT,
@@ -46,13 +61,23 @@ struct Token
     // ^ add new keywords ^
     NUMBER,
     ID,
-    
+
     ind_size,
   };
-  int ind = UNDEFINED;
+  int ind = -1;
+  int err = ERR_TOKEN_UNDEFINED;
 };
 
-int tokenize2(std::istream &ss, std::ostream &os_tokens,
+struct TokenStack
+{
+  virtual ~TokenStack() = default;
+  virtual void push_keyword(int col, int row, int keyword) = 0;
+  virtual void push_err(int col, int row, int err) = 0;
+  virtual void push_num(int col, int row, const std::string &str_num) = 0;
+  virtual void push_id(int col, int row, const std::string &str_id) = 0;
+};
+
+int tokenize2(std::istream &ss, TokenStack &token_stack,
               int verbose_level = 0) {
   using std::array;
   using std::cout;
@@ -62,26 +87,17 @@ int tokenize2(std::istream &ss, std::ostream &os_tokens,
   using std::max_element;
   using std::string;
 
-  constexpr int n_keywords = 24;
-  constexpr int n_tokens = n_keywords + 2;
-  constexpr int ind_token_num = n_keywords + 0;
-  constexpr int ind_token_id =  n_keywords + 1;
-  const string token_id_stops = "+=!(){}?:;&|-*/";
-  const auto is_id = [&token_id_stops](const char c) { return token_id_stops.find(c) == string::npos; };
-  const array<string, n_keywords> token_strs{
-      "+", "+=", "!",    "!=",     "=",  "==",    "(",     ")",
-      "{", "}",  "func", "return", "if", "while", "print", "else",
-      "?", ":",  ";",    "&&",     "-",  "*",     "/",     "||",
-  };
   array<int, n_tokens> token_matches;
   array<int, n_tokens> token_matches_next;
   fill(token_matches.begin(), token_matches.end(), 0);
 
   string s;
+  int row = 0;
   while (getline(ss, s)) {
+    ++row;
     const int len = s.size();
     if (verbose_level >= 2)
-      cout << "next is line '" << s << "' (" << len << ")\n";
+      cout << "next is line " << row << "'" << s << "' (" << len << ")\n";
     /// `i` is pos in str
     /// `t` is pos in token_strs
     bool has_any_token = false;
@@ -118,7 +134,7 @@ int tokenize2(std::istream &ss, std::ostream &os_tokens,
       {
         int &len_match_num = token_matches_next[ind_token_num];
         // can't start from 0
-        if (len_match_num >= 0 && isdigit(c) && !(len_match_num == 0 && c == '0')) {
+        if (len_match_num >= 0 && isdigit(c)) {
           ++len_match_num;
           if (verbose_level >= 3)
             cout << "  inc number to " << len_match_num << "\n";
@@ -193,23 +209,30 @@ int tokenize2(std::istream &ss, std::ostream &os_tokens,
       }
       if (verbose_level >= 1)
         cout << "it's a...";
-      if (is_ambigious) {
-        if (verbose_level >= 1)
-          cout << " ambigious call!\n";
-      } else if (token_ind_best == -1) {
+      
+      const bool ruined_all = (-1 == *max_element(token_matches_next.begin(), token_matches_next.end()));
+      const int token_col = 2 + i + ruined_all - token_match_best;
+      const int token_row = row;
+
+      if (token_ind_best == -1) {
+        token_stack.push_err(token_row, token_col, ERR_TOKEN_UNDEFINED);
         if (verbose_level >= 1)
           cout << " undefined token!\n";
+      } else if (is_ambigious) {
+        token_stack.push_err(token_row, token_col, ERR_TOKEN_AMBIGIOUS);
+        if (verbose_level >= 1)
+          cout << " ambigious call!\n";
       } else if (token_ind_best < ind_token_num) {
-        os_tokens << token_strs[token_ind_best] << "\n";
+        token_stack.push_keyword(token_row, token_col, token_ind_best);
         if (verbose_level >= 1)
           cout << " '" << token_strs[token_ind_best] << "'\n";
       } else {
-        // if rollbacked, then inc pos back to substr
-        const bool ruined_all = (-1 == *max_element(token_matches_next.begin(), token_matches_next.end()));
-        const int str_end = i + 1 + ruined_all;
-        const int str_len = token_match_best;
-        const string substr = s.substr(str_end - str_len, str_len);
-        os_tokens << substr << "\n";
+        const string substr = s.substr(token_col - 1, token_match_best);
+        if (token_ind_best == ind_token_num)
+          token_stack.push_num(token_row, token_col, substr);
+        if (token_ind_best == ind_token_id)
+          token_stack.push_id(token_row, token_col, substr);
+
         if (token_ind_best == ind_token_num && verbose_level >= 1)
           cout << " number " << substr << "\n";
         else if (token_ind_best == ind_token_id && verbose_level >= 1)
@@ -226,12 +249,75 @@ int tokenize2(std::istream &ss, std::ostream &os_tokens,
   return ERR_NO;
 }
 
-std::pair<int, std::string> tokenize_string(std::string const &str) {
+int tokenize_string(std::string const &str, TokenStack &token_stack, int verbose_level = 0) {
   std::stringstream in(str);
-  std::stringstream out;
-  const int code = tokenize2(in, out);
-  return {code, out.str()};
+  return tokenize2(in, token_stack, verbose_level);
 }
+
+struct TokenStackOutStream final : TokenStack
+{
+  TokenStackOutStream(std::ostream &out, int verbose_level = 0) : 
+    out_(out), verbose_level_(verbose_level)
+  {}
+  inline void push_keyword(int, int, int keyword) override {
+    out_ << token_strs[keyword] << "\n";
+  }
+  inline void push_err(int, int, int err) override {
+    if (verbose_level_ > 0)
+      out_ << "err code ";
+    else
+      out_ << "!";
+    out_ << err << "\n";
+  }
+  inline void push_num(int, int, const std::string &str_num) override {
+    if (verbose_level_ > 0)
+      out_ << "number '"; 
+    out_ << str_num;
+    if (verbose_level_ > 0)
+      out_ << "'";
+    out_ << "\n";
+  }
+  inline void push_id(int, int, const std::string &str_id) override {
+    if (verbose_level_ > 0)
+      out_ << "number '"; 
+    out_ << str_id;
+    if (verbose_level_ > 0)
+      out_ << "'";
+    out_ << "\n";
+  }
+private:
+  std::ostream &out_;
+  int verbose_level_ = 0;
+};
+
+struct TokenStack2 final : TokenStack
+{
+  inline void push_keyword(int col, int row, int keyword) override {
+    tokens_.push_back(Token{"", col, row, keyword, ERR_NO});
+  }
+  inline void push_err(int col, int row, int err) override {
+    tokens_.push_back(Token{"", col, row, -1, err});
+  }
+  inline void push_num(int col, int row, const std::string &str_num) override {
+    tokens_.push_back(Token{str_num, col, row, Token::NUMBER, ERR_NO});
+  }
+  inline void push_id(int col, int row, const std::string &str_id) override {
+    tokens_.push_back(Token{str_id, col, row, Token::ID, ERR_NO});
+  }
+  inline bool has_next() const { 
+    return pos_ < tokens_.size();
+  }
+  inline Token pop() { 
+    if (!has_next()) {
+      throw std::runtime_error("no tokens to pop"); 
+      return {}; 
+    }
+    return tokens_[pos_++];
+  }
+private:
+  std::vector<Token> tokens_;
+  int pos_ = 0;
+};
 
 std::pair<int, std::string> tokenize_test_in(std::string const &filename,
                                              int verbose_level = 0) {
@@ -240,7 +326,8 @@ std::pair<int, std::string> tokenize_test_in(std::string const &filename,
   if (!in)
     return {ERR_CANT_READ_FILE, ""};
   std::stringstream out;
-  const int code = tokenize2(in, out, verbose_level);
+  TokenStackOutStream tokens_out(out);
+  const int code = tokenize2(in, tokens_out, verbose_level);
   in.close();
   return {code, out.str()};
 }
@@ -266,7 +353,7 @@ std::pair<int, std::string> read_test_out(std::string const &filename) {
   return {code, ss.str()};
 }
 
-int run_test(const std::string &num) {
+int run_lexer_test(const std::string &num) {
   const bool verbose = true;
   if (verbose)
     std::cout << "Test '" << num << "': ";
@@ -291,22 +378,25 @@ int run_test(const std::string &num) {
 }
 
 int main() {
-  run_test("001");
-  run_test("002");
-  run_test("003");
-  run_test("004");
-  run_test("005");
-  run_test("006");
-  run_test("007");
-  run_test("008");
-  run_test("009"); 
-
-  run_test("010"); // nums complex
-  run_test("011"); // nums w/ spaces
-  run_test("012"); // nums w/o spaces
-  run_test("013"); // ids
-  run_test("014"); // ids vs keywords
-  run_test("015"); // ids and nums w/o space
+  run_lexer_test("001");
+  run_lexer_test("002");
+  run_lexer_test("003");
+  run_lexer_test("004");
+  run_lexer_test("005");
+  run_lexer_test("006");
+  run_lexer_test("007");
+  run_lexer_test("008");
+  run_lexer_test("009");
+  run_lexer_test("010"); // nums complex
+  run_lexer_test("011"); // nums w/ spaces
+  run_lexer_test("012"); // nums w/o spaces
+  run_lexer_test("013"); // ids
+  run_lexer_test("014"); // ids vs keywords
+  run_lexer_test("015"); // ids and nums w/o space
+  run_lexer_test("016"); // nums w/ zeros
+ 
+  TokenStackOutStream stack(std::cout, 10);
+  tokenize_string("1+002+3", stack, 0);
 
   return 0;
 }
